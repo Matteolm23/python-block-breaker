236b29693c1ddc12e837789509d92a50b699bb86
Matteolm23 <matteo.lamalfa@davincimilazzo.edu.it>
2026-05-16 18:34:08 +0200

    Update blockbreaker2.py

--- blockbreaker2.py
+++ blockbreaker2.py
@@ -2,7 +2,7 @@ import pygame as g
 import random as r
 import sys as s
 from os import path, chdir
-from math import cos,sin
+from math import sin,cos,pi
 
 chdir(path.dirname(path.realpath(__file__)))
 g.init()
@@ -11,56 +11,102 @@ WIDTH, HEIGHT = 720,720
 WIN = g.display.set_mode((WIDTH,HEIGHT))
 screen_rect = WIN.get_rect()
 
+blocksprites = [
+    g.image.load('sprites/greenblock.png'),
+    g.image.load('sprites/redblock.png'),
+    g.image.load('sprites/cyanblock.png'),
+    g.image.load('sprites/goldblock.png'),
+    g.image.load('sprites/purpleblock.png')
+]
+
+def degtorad(angle):
+    return angle * (pi/180)
+
+def drawtext(text,size,color,centerx,centery):
+    font = g.font.Font('HERCULESPIXELFONTREGULAR-OVAX0.OTF', size)
+    rtext = font.render(str(text),True,color)
+    textrect = rtext.get_rect(center = (centerx, centery))
+    WIN.blit(rtext, textrect)
+
+def checkAABBCollision(A, B):
+    AisToTheRightOfB = A.left > B.right
+    AisToTheLeftOfB = A.right < B.left
+    AisAboveB = A.bottom < B.top
+    AisBelowB = A.top > B.bottom
+    return not (AisToTheRightOfB or AisToTheLeftOfB or AisAboveB or AisBelowB)
+
 class BALL():
-    size = 30
-    spd = 6
-    vel = [spd,spd]
-    collidecooldown = 0
+    size = 35
+    start = False
+    spd = 7
+    vel = [spd*cos(0),spd*sin(0)]
     alive = True
-    sprite = g.transform.scale(g.image.load('bricks-wip.png').convert_alpha(), (size,size))
+    sprite = g.transform.scale(g.image.load('sprites/ball.png').convert_alpha(), (size,size))
     
     def __init__(self):
-        self.x = 120#WIDTH/2 - self.size/2
+        self.x = WIDTH/2 - self.size/2
         self.y = HEIGHT*.85
+        self.killcooldown = 0
     
     def step(self,paddle):
 
-        self.collidecooldown-=1
+        self.killcooldown -= 0
 
-        myrect = g.Rect(self.x,self.y,self.size,self.size)
-        paddlerect = g.Rect(paddle.x,paddle.y,paddle.width,paddle.height)
+        if not BALL.start:
+            self.x = paddle.x + paddle.width/2 - self.size/2
 
-        if myrect.colliderect(paddlerect) and self.collidecooldown < 1:
-            self.vel[1]*=-1
-            self.collidecooldown = 3
+            keys = g.key.get_pressed()
+            if keys[g.K_UP] or keys[g.K_w]:
+                angle = 40
+                if keys[g.K_a] or keys[g.K_LEFT]:
+                    angle *= -1
+                rad = degtorad(angle-90)
+                self.vel[0] = self.spd*cos(rad)
+                self.vel[1] = self.spd*sin(rad)
+                BALL.start = True
+        else:
+            myrect = g.Rect(self.x+self.vel[0],self.y+self.vel[1],self.size+self.vel[0],self.size+self.vel[1])
+            paddlerect = g.Rect(paddle.x,paddle.y,paddle.width,paddle.height)
 
-        if self.x + self.size + self.vel[0] > WIDTH or self.x + self.vel[0] < 0:
-            self.vel[0]*=-1
-        if self.y + self.vel[1] < 0 or self.y + self.vel[1] + self.size > HEIGHT:
-            self.vel[1]*=-1
-            
-        #if self.y + self.yspd > HEIGHT:
-        #    self.alive = False
+            if myrect.colliderect(paddlerect):
+                angle = myrect.centerx - paddlerect.centerx
+                angle = min(angle, 70)
+                angle = max(angle, -70)
+                if angle > 0 and angle < 5:
+                    angle = 5
+                if angle < 0 and angle > -5:
+                    angle = -5
+                rad = degtorad(angle-90)
+                self.vel[0] = self.spd*cos(rad)
+                self.vel[1] = self.spd*sin(rad)
 
-        self.x += self.vel[0]
-        self.y += self.vel[1]
+            if self.x + self.size + self.vel[0] > WIDTH or self.x + self.vel[0] < 0:
+                self.vel[0]*=-1
+            if self.y + self.vel[1] < 0 or self.y + self.vel[1] + self.size > HEIGHT:
+                self.vel[1]*=-1
+                
+            if self.y + self.vel[1] > HEIGHT:
+                self.alive = False
 
-        myrect.clamp_ip(screen_rect)
+            self.x += self.vel[0]
+            self.y += self.vel[1]
+
+            myrect.clamp_ip(screen_rect)
 
     def draw(self):
         WIN.blit(self.sprite,g.Rect(self.x,self.y,self.size,self.size))
 
 class PADDLE():
-    width = 160
+    width = 125
     height = 15
-    spd = 6
-    sprite = g.transform.scale(g.image.load('bricks-wip.png').convert_alpha(), (width,height))
+    spd = 8
 
     def __init__(self):
         self.x = WIDTH/2 - self.width/2
         self.y = HEIGHT*.9
 
     def step(self):
+
         keys = g.key.get_pressed()
         if (keys[g.K_LEFT] or keys[g.K_a]):
             if self.x - self.spd > 0:
@@ -70,83 +116,87 @@ class PADDLE():
                 self.x += self.spd
 
     def draw(self):
-        WIN.blit(self.sprite,g.Rect(self.x,self.y,self.width,self.height))
+        g.draw.rect(WIN,(255,255,255),g.Rect(self.x,self.y,self.width,self.height))
+        #WIN.blit(self.sprite,g.Rect(self.x,self.y,self.width,self.height))
 
 class BLOCK():
     width = 75
     height = 40
-    sprite = g.transform.scale(g.image.load('greenblock.png').convert_alpha(), (width,height))
+    deathcooldown = 0
 
     def __init__(self,xx,yy):
+        self.sprite = g.transform.scale(blocksprites[r.randint(0,4)].convert_alpha(), (self.width,self.height))
         self.x = xx
         self.y = yy
         self.alive = True
+        self.goindown = 260
 
     def step(self,ball):
 
+        ball.killcooldown -= 1
+
+        if self.goindown > 1:
+            spd = self.goindown/25
+            self.goindown -= spd
+            self.y += spd
+
         myrect = g.Rect(self.x,self.y,self.width,self.height)
-        ballrect = g.Rect(ball.x,ball.y,ball.size,ball.size)
+        ballrect = g.Rect(ball.x+ball.vel[0],ball.y+ball.vel[1],ball.size+ball.vel[0],ball.size+ball.vel[1])
 
-        if myrect.colliderect(ballrect) and ball.collidecooldown < 1:
-            ctop = ball.y - myrect.bottom 
-            cbottom = self.y - ballrect.bottom
-            cleft = ball.x - myrect.right
-            cright = self.x - ballrect.right
+        if myrect.colliderect(ballrect) and ball.killcooldown < 1:
+            cbottom = abs(myrect.bottom - ballrect.top)
+            ctop = abs(myrect.top - ballrect.bottom)
+            cright = abs(myrect.right - ballrect.left)
+            cleft = abs(myrect.left - ballrect.right)
 
-            cy = abs(min(ctop,cbottom))
-            cx = abs(min(cleft,cright))
+            cy = min(ctop,cbottom)
+            cx = min(cleft,cright)
 
             if cy < cx:
                 ball.vel[1]*=-1
             else:
                 ball.vel[0]*=-1
 
-            self.alive = False 
-            ball.collidecooldown = 1
-        """
-            ctop = ballrect.bottom > myrect.top and ballrect.bottom < myrect.bottom
-            cbottom = ballrect.top < myrect.bottom and myrect.top > ballrect.bottom
-            cright = ballrect.left < myrect.right and ballrect.left > myrect.left
-            cleft = ballrect.right > myrect.left and ballrect.right < myrect.right
-
-            if ctop or cbottom:
-                ball.vel[1]*=-1
-            if cright or cleft:
-                ball.vel[0]*=-1
-
-            self.alive = False 
-            ball.collidecooldown = 1
-        """
-        
+            self.alive = False
+            ball.killcooldown = 2
 
     def draw(self):
         WIN.blit(self.sprite,g.Rect(self.x,self.y,self.width,self.height))
 
 class LOGIC():  
 
+    blocks = []
     br = 4
     bc = 7
-    spacing = 5
-    offset = (WIDTH-((BLOCK.width+spacing)*bc))/2
+
+    def blockspawner(self,blocklist,br,bc,spacing):
+        offset = (WIDTH-((BLOCK.width+spacing)*bc))/2
+        blocklist.clear()
+        for i in range(bc):
+            blocklist.append(list(BLOCK(i*(BLOCK.width+spacing)+offset,j*(BLOCK.height+spacing)+offset-260) for j in range(br)))      
 
     def __init__(self):
-        self.blocks = []
-        for i in range(self.bc):
-            self.blocks.append(list(BLOCK(i*(BLOCK.width+self.spacing)+self.offset,j*(BLOCK.height+self.spacing)+self.offset/1.2) for j in range(self.br)))
-
+        self.blockspawner(self.blocks,self.br,self.bc,5)
         self.paddle = PADDLE()
         self.ball = BALL()
 
     def step(self):
-        self.paddle.step()
+        respawnblocks = True
         for i in range(self.bc):
             for j in range(self.br):
                 if self.blocks[i][j].alive:
+                    respawnblocks = False
                     self.blocks[i][j].step(self.ball)
+
+        if respawnblocks:
+            BALL.spd += 1
+            self.blockspawner(self.blocks,self.br,self.bc,5)
+
+        self.paddle.step()
         self.ball.step(self.paddle)
 
     def draw(self):
-        WIN.fill((0,0,0))
+        WIN.fill((15,0,25))
         for i in range(self.bc):
             for j in range(self.br):
                 if self.blocks[i][j].alive:
